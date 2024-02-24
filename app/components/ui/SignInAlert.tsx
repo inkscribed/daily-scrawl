db4cbd862139292438f1b4be4ea7e455870d5731
Bara Kona <bkonateh1@gmail.com>
2024-02-24 18:34:07 +0100

    Make signInAlert server component

--- app/components/ui/SignInAlert.tsx
+++ app/components/ui/SignInAlert.tsx
@@ -1,22 +1,42 @@
-"use client";
+import "./globals.css";
+import "@mantine/tiptap/styles.css";
+import "@mantine/core/styles.css";
+
 import { Alert } from "@mantine/core";
-import { useTheme } from "next-themes";
-import { FC, ReactNode } from "react";
+import Link from "next/link";
 import { IconInfoCircle } from "@tabler/icons-react";
 
-export const SignInAlert: FC<{ children: ReactNode }> = ({ children }) => {
-	const { theme } = useTheme();
+export const SignInAlert = () => {
 	return (
 		<Alert
 			variant="light"
-			color={theme === "dark" ? "violet" : "dark"}
 			title="You are not signed in!"
 			icon={<IconInfoCircle />}
 			classNames={{
 				message: "!flex flex-col gap-4",
+				body: "dark:!bg-background !bg-text dark:text-text",
+				root: "dark:!bg-background !bg-text",
+				wrapper: "dark:bg-background bg-text",
+				label: "dark:text-text text-background",
+				closeButton: "dark:text-text text-background",
+				icon: "dark:text-text text-background",
+				title: "dark:text-text text-background",
 			}}
 		>
-			{children}
+			<p className="text-background dark:text-text">
+				Please sign in to access these features.
+				<br />
+				You will be able to view your past writing and track your consistency as
+				well as export to various formats.
+			</p>
+			<div className="ml-auto mb-2">
+				<Link
+					href="/sign-in"
+					className="ml-auto text-sm hover:dark:bg-hoverLight hover:bg-hoverDark dark:bg-text dark:text-background bg-background text-text duration-300 transition-all ease-in-out rounded px-3 py-1"
+				>
+					Sign in
+				</Link>
+			</div>
 		</Alert>
 	);
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,17 @@ import { ClerkProvider, auth } from "@clerk/nextjs";
 import { Navbar } from "./components/navigation/Navbar";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
 import { dark } from "@clerk/themes";
-import { SignInAlert } from "./components/ui/SignInAlert";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@mantine/core/styles.css";
 
-import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { Affix, ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { Details } from "./components/modals/Details";
 import { Scrawls } from "./components/scrawls/Scrawls";
 import { Introduction } from "./components/modals/Introduction";
 import Link from "next/link";
-import { DetailsAffix } from "./components/modals/DetailsAffix";
 import { IconBooks } from "@tabler/icons-react";
+import { SignInAlert } from "./components/ui/SignInAlert";
 
 export const metadata: Metadata = {
 	title: "Daily Scrawl",
@@ -55,34 +54,15 @@ export default async function RootLayout({
 							<Navbar />
 							{children}
 							<Details>
-								{!userId ? (
-									<SignInAlert>
-										<p className="text-background dark:text-text">
-											Please sign in to access these features.
-											<br />
-											You will be able to view your past writing and track your
-											consistency as well as export to various formats.
-										</p>
-										<div className="ml-auto mb-2">
-											<Link
-												href="/sign-in"
-												className="ml-auto text-sm hover:dark:bg-hoverLight hover:bg-hoverDark dark:bg-text dark:text-background bg-background text-text duration-300 transition-all ease-in-out rounded px-3 py-1"
-											>
-												Sign in
-											</Link>
-										</div>
-									</SignInAlert>
-								) : (
-									<Scrawls userId={userId} />
-								)}
+								{!userId ? <SignInAlert /> : <Scrawls userId={userId} />}
 							</Details>
-							<DetailsAffix>
+							<Affix position={{ bottom: 20, left: 20 }} zIndex={1}>
 								<Link href="?details=true" prefetch={true}>
 									<button className="p-2 border border-lightBorder dark:border-border rounded-md bg-primary-500 text-background dark:text-text hover:bg-hoverLight dark:hover:bg-hoverDark dark:bg-background bg-text duration-300 transition-all ease-in-out">
 										<IconBooks size={22} />
 									</button>
 								</Link>
-							</DetailsAffix>
+							</Affix>
 							<Introduction />
 						</MantineProvider>
 					</ThemeProvider>

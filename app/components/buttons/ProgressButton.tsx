9597fd497a679870a4a76ce2f04149d601250de5
Bara Kona <bkonateh1@gmail.com>
2024-03-11 13:30:27 +0100

    Migrate from planetscale -> railway SQL

--- app/components/buttons/ProgressButton.tsx
+++ app/components/buttons/ProgressButton.tsx
@@ -8,7 +8,7 @@ export const ProgressButton = () => {
 	return (
 		<button onClick={open} className="mx-auto">
 			<AnimatedArrowButton className="mt-6 flex items-center mx-auto bg-background hover:bg-hoverDark dark:bg-textDark dark:text-background text-text dark:hover:bg-hoverLight rounded-full py-3 px-5 font-semibold">
-				<p className="-mt-0.5">See your progress</p>
+				<p className="mt-0.5">See your progress</p>
 			</AnimatedArrowButton>
 		</button>
 	);
